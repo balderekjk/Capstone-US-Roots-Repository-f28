276a8a142eae8218e4a1ed5ecc71f5d19ed07762
Derek Bal <bal.derek.j@gmail.com>
2022-10-07 09:05:54 -1000

    updated with corsoptions

--- server/index.js
+++ server/index.js
@@ -4,8 +4,14 @@ const path = require('path');
 
 const app = express();
 
+const corsOptions = {
+  origin: 'https://us-roots-repository-server.herokuapp.com/',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, '../client')));
 
