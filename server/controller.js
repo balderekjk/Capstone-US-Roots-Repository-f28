4f07818a31316074a951d7045328af0402077e09
Derek Bal <bal.derek.j@gmail.com>
2023-01-02 08:34:43 -1000

    reverted to pre-alias feature

--- server/controller.js
+++ server/controller.js
@@ -2,13 +2,12 @@ let bookmarks = require('./db.json');
 
 module.exports = {
   storeBookmarkObj: (req, res) => {
-    const { id, page, sectionId, alias } = req.body;
+    const { id, page, sectionId } = req.body;
     if (bookmarks.findIndex((bookmark) => bookmark['id'] === id) === -1) {
       let newBookmark = {
         id: id,
         page: page,
         sectionId: sectionId,
-        alias: alias,
       };
       bookmarks.push(newBookmark);
       res.status(200).send(bookmarks);
